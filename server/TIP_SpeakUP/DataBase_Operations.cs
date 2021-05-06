578b2d33d3ef35fef69e3932ffbb199fde5f74a3
wojciooo <wojci123@gmail.com>
2021-05-06 23:09:09 +0200

    Dodanie bazy danych użytkowników

---
+++ server/TIP_SpeakUP/DataBase_Operations.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+namespace TIP_SpeakUP
+{
+    public class DataBase_Operations
+    {
+
+        SQLiteConnection database;
+        public DataBase_Operations(string cs)
+        {
+            database = new SQLiteConnection(cs);
+
+
+            SQLiteCommand com = new SQLiteCommand(database);
+            database.Open();
+            com.CommandText = "CREATE TABLE IF NOT EXISTS USERS (ID INTEGER PRIMARY KEY AUTOINCREMENT,username varchar(255),password varchar(255))";
+            com.ExecuteNonQuery();
+            database.Close();
+        }
+
+
+        public List<string> get_accounts()
+        {
+            database.Open();
+            SQLiteCommand com = database.CreateCommand();
+            SQLiteDataReader sqlite_datareader;
+            List<string> accounts = new List<string>();
+            com.CommandText = "SELECT * FROM USERS";
+
+            sqlite_datareader = com.ExecuteReader();
+
+            while (sqlite_datareader.Read())
+            {
+                accounts.Add($"{sqlite_datareader.GetString(1)}");
+            }
+            database.Close();
+            //zwraca wszystkie konta
+            return accounts;
+        }
+
+        public string add_account(string username, string password)
+        {
+
+            database.Open();
+            SQLiteCommand com = new SQLiteCommand(database);
+            com.CommandText = "INSERT INTO USERS(username, password) VALUES(@username, @password)";
+            com.Parameters.AddWithValue("@username", username);
+            com.Parameters.AddWithValue("@password", password);
+            com.Prepare();
+            try
+            {
+                com.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            database.Close();
+            //dodawanie konta
+            return "OK";
+        }
+
+
+
+        public string[] get_account(string username)
+        {
+            database.Open();
+            SQLiteCommand com = database.CreateCommand();
+            SQLiteDataReader sqlite_datareader;
+            string[] account = new string[2];
+            com.CommandText = "SELECT * FROM USERS WHERE username = @username";
+            com.Parameters.AddWithValue("@username", username);
+
+            sqlite_datareader = com.ExecuteReader();
+
+            while (sqlite_datareader.Read())
+            {
+                account[0] = sqlite_datareader.GetString(1);
+                account[1] = sqlite_datareader.GetString(2);
+            }
+            database.Close();
+            //zwraca konto / jezeli pusty array to nie istnieje
+            return account;
+        }
+
+    }
+}
--- server/TIP_SpeakUP/Program.cs
+++ server/TIP_SpeakUP/Program.cs
@@ -8,28 +8,39 @@ namespace TIP_SpeakUP
     {
         static void Main(string[] args)
         {
-           /* Dictionary<string, List<string>> xd = new Dictionary<string, List<string>>();
-            xd.Add("aaa", new List<string>());
-            xd.Add("bbb", new List<string>());
-            xd["aaa"].Add("helou");
-            xd["aaa"].Add("helou2");
-            xd["aaa"].Add("helou3");
-            xd["bbb"].Add("xd123");
-            xd["bbb"].Add("xd124");
+            /* Dictionary<string, List<string>> xd = new Dictionary<string, List<string>>();
+             xd.Add("aaa", new List<string>());
+             xd.Add("bbb", new List<string>());
+             xd["aaa"].Add("helou");
+             xd["aaa"].Add("helou2");
+             xd["aaa"].Add("helou3");
+             xd["bbb"].Add("xd123");
+             xd["bbb"].Add("xd124");
 
-            string ans = "LST$$";
-            foreach(var e in xd)
-            {
-                ans += e.Key;
-                ans += "$$";
-                e.Value.ForEach(x=>ans+=x+"$$");
-            }
-            Console.WriteLine(ans);*/
+             string ans = "LST$$";
+             foreach(var e in xd)
+             {
+                 ans += e.Key;
+                 ans += "$$";
+                 e.Value.ForEach(x=>ans+=x+"$$");
+             }
+             Console.WriteLine(ans);*/
 
             //starting server
-            Server serwer = new Server();
-            Console.WriteLine("witam");
-            Console.ReadKey();
+            //   Server serwer = new Server();
+            //  Console.WriteLine("witam");
+            //  Console.ReadKey();
+
+
+
+            DataBase_Operations db = new DataBase_Operations("URI=FILE:Users.db");
+            //db.add_account("witam", "nara");
+            //db.add_account("witam3", "nara");
+            //db.add_account("witam4", "nara");
+            //db.add_account("witam5", "nara");
+            //db.get_accounts().ForEach(Console.WriteLine);
+            Console.WriteLine(db.get_account("witam2345")[0]);
+
         }
     }
 }
