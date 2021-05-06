a49ce051bfe6ac4ebf52ac643324504208fbf1c1
wojciooo <wojci123@gmail.com>
2021-05-06 19:17:34 +0200

    smoll update

--- server/TIP_SpeakUP/Functions.cs
+++ server/TIP_SpeakUP/Functions.cs
@@ -6,5 +6,14 @@ namespace TIP_SpeakUP
 {
     class Functions
     {
+        /// <summary>
+        ///  function realizing client requests
+        /// </summary>
+        /// <param name="OP">Operation requested by client</param>
+        /// <returns></returns>
+        public static string DecodeOperation(string OP)
+        {
+            return "OK";
+        }
     }
 }
