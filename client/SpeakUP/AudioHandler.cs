c1011a262719b1b0e7f71e3b163979cf1f6ccb22
Gregory Matczak <grzesmatczak@gmail.com>
2021-06-28 16:20:45 +0200

    Added SHA256

--- client/SpeakUP/AudioHandler.cs
+++ client/SpeakUP/AudioHandler.cs
@@ -77,7 +77,7 @@ namespace SpeakUP
 
         private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
         {
-            Parallel.ForEach(OnCall, ip => { SendSound(e.Buffer, ip); });
+            foreach (string ip in OnCall) { Task.Run(() => SendSound(e.Buffer, ip)); }
             // TestBuffer.AddSamples(e.Buffer, 0, e.BytesRecorded)
 
         }
